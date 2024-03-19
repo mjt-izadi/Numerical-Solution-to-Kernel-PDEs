8f90a917a10b4df3e7ab6d96df0f76d1d2fe7b5e
Mojtaba Izadi <mjt.izadi@gmail.com>
2024-03-19 02:15:51 -0600

    added calculate_gain method

--- kernel_PDE.py
+++ kernel_PDE.py
@@ -3,10 +3,9 @@ import matplotlib.pyplot as plt
 
 class Kernel_parabolic:
     # Backstepping kernel for parabolic PDE
-    lam = 10.0
-    c = 0.0
-
-    def __init__(self, N):
+    def __init__(self, lam, c, N):
+        self.lam = lam
+        self.c = c
         self.N = N
         self.h = 1 / (self.N - 1)
         self.xi = np.linspace(0, 2, 2 * N - 1)
@@ -33,16 +32,22 @@ class Kernel_parabolic:
                         A[self.indices[i, j], self.indices[m, n + 1]] += const * self.h ** 2 / 4
                         A[self.indices[i, j], self.indices[m + 1, n]] += const * self.h ** 2 / 4
                         A[self.indices[i, j], self.indices[m + 1, n + 1]] += const * self.h ** 2 / 4
-        # print(A[10])
-        # print(b[10])
         self.kernel = np.linalg.solve(A, b)
+
+    def calculate_gain(self):
+        ij = np.array([[self.N + n - 1, self.N - n - 1] for n in range(self.N)])
+        self.y = (self.xi[ij[:, 0]] - self.eta[ij[:, 1]]) / 2
+        gain_indices = [self.indices[i, j] for i, j in ij]
+        self.gain = self.kernel[gain_indices]            
+
 
 if __name__ == "__main__":
     N = 50
-    k1 = Kernel_parabolic(N)
+    k1 = Kernel_parabolic(25.0, 0.0, N)
     k1.calculate_kernel_xieta()
+    k1.calculate_gain()
 
     fig = plt.figure()
     ax1 = fig.add_subplot(111)
-    ax1.plot(k1.kernel)
+    ax1.plot(k1.y, k1.gain)
     plt.show()
